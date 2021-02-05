bedafa046ec2d06e2a8417dda171106c5343772e
Jonathan Ly <windevly@gmail.com>
2021-02-05 12:06:21 -0600

    integrates video of wishing, fixes reset not resetting banner, and fades in banner

--- src/features/wish/Wish.tsx
+++ src/features/wish/Wish.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@chakra-ui/react';
-import React from 'react';
+import { Button, Image } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import PATHS from '../../constants/Paths';
+import { Item } from '../../models/Item';
 import { useWishState } from '../../state-management/store';
 
 interface WishProps {
@@ -8,26 +9,49 @@ interface WishProps {
 }
 
 const Wish = (props: WishProps) => {
+    const [hasVideoEnded, setHasVideoEnded] = useState(false);
     const state = useWishState();
     const { setIsWishing } = props;
 
     const getImageSource = (name: string): string => `${PATHS.CHARACTER_WISH_IMAGES}/character_${name.toLowerCase()}.png`;
 
+    const getVideoPath = () => {
+        const hasFiveStar = state.results.some((item: Item) => item.stars === 5);
+
+        if (hasFiveStar)
+            return `${PATHS.VIDEOS}/five_star.mp4`;
+        
+        const hasFourStar = state.results.some((item: Item) => item.stars === 4);
+
+        if (hasFourStar)
+            return `${PATHS.VIDEOS}/four_star.mp4`;
+        
+        return `${PATHS.VIDEOS}/three_star.mp4`;
+    }
+
     return (
         <>
             {
-                state.results.map((result, index) => {
+                !hasVideoEnded ?
+                (
+                    <video autoPlay={true} onEnded={() => setHasVideoEnded(true)}>
+                        <source src={getVideoPath()}/>
+                    </video>
+                ) : null
+            }
+            {
+                hasVideoEnded ? state.results.map((result, index) => {
                     return (
                         <div key={result.name + index}>
                             <h1>{result.name}</h1>
-                            <img src={getImageSource(result.name)} alt={`IMAGE-${result.name}`}/>
+                            <Image src={getImageSource(result.name)} alt={`IMAGE-${result.name}`}/>
                         </div>
                     )
-                })
+                }) : null
             }
             <Button onClick={() => setIsWishing(false)}>Go Back</Button>
         </>
-    )
+    );
 }
 
 export default Wish;
